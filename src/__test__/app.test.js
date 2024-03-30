48a89acfddddcf203740e837434169ad8166530f
Grigory <weissman1979@gmail.com>
2024-03-30 20:48:35 +0400

    commit

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -3,15 +3,15 @@ import Magician from "../magician";
 
 test('Атака мага без дурмана', () => {
 	const Stas = new Magician('Stas');
-	ivan.attack = 100;
+	Stas.attack = 100;
 	const result = Stas.attack;
 	expect(result(2)).toEqual(90);
 });
 
 test("Атака демона под дурманом", () => {
 	const German = new Daemon("German");
-	maxim.attack = 100;
-	maxim.stoned = true;
+	German.attack = 100;
+	German.stoned = true;
 	const result = German.attack;
 	expect(result(2)).toEqual(85);
 });
